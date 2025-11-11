ba82b58d7f37c0a5441ee1941fd4beb67d60f553
Wang929929 <124090701@link.cuhk.edu.cn>
2025-11-11 10:41:28 +0800

    Add files via upload

---
+++ sunflower.cpp
@@ -0,0 +1,24 @@
+#include "sunflower.h"
+#include "sun.h"
+#include <QDebug>
+
+SunFlower::SunFlower()
+{
+    hp = 300;
+    time = int(10.0 * 1000 / 33);
+    setMovie(":/image/plant/SunFlower.gif");
+}
+
+void SunFlower::advance(int phase)
+{
+    if (!phase)
+        return;
+    update();
+    if (hp <= 0)
+        delete this;
+    else if (++counter >= time)
+    {
+        counter = 0;
+        scene()->addItem(new Sun(pos()));
+    }
+}
