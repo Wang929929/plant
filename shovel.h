dbccb20f9381e69cc978f0639fad7a4c70767ec4
Wang929929 <124090701@link.cuhk.edu.cn>
2025-11-11 17:26:12 +0800

    Add Shovel class with graphics item functionality

--- shovel.h
+++ shovel.h
@@ -1,4 +1,25 @@
 #ifndef SHOVEL_H
 #define SHOVEL_H
 
+#include <QGraphicsItem>
+#include <QString>
+#include <QGraphicsSceneMouseEvent>
+#include <QCursor>
+#include <QApplication>
+#include <QDrag>
+#include <QMimeData>
+#include <QWidget>
+#include <QPainter>
+
+class Shovel : public QGraphicsItem{
+public:
+    Shovel();
+    QRectF boundingRect() const override;
+    void paint(QPainter *painter, const QStyleOptionGraphicsItem *option, QWidget *widget) override;
+    void mousePressEvent(QGraphicsSceneMouseEvent *event) override;
+    void mouseMoveEvent(QGraphicsSceneMouseEvent *event) override;
+    void mouseReleaseEvent(QGraphicsSceneMouseEvent *event) override;
+    void removePlant(QPointF pos);
+};
+
 #endif // SHOVEL_H
