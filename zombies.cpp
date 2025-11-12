84aad410dc3225917c9f1855acbec71594f4b1c7
tujili067 <124090593@link.cuhk.edu.cn>
2025-11-12 18:49:46 +0800

    Update zombies.cpp

--- zombies.cpp
+++ zombies.cpp
@@ -1,179 +1,206 @@
 #include "zombies.h"
-#include <QPainter>
-#include <QMap>
-#include <QDebug>
-#include "shop.h"
 #include "mainwindow.h"
-Zombies::Zombies(QString name,bool isStand,QGraphicsScene * scene,QObject * parent)
+#include <QDebug>
+#include <QTimer>
+#include <QMap>
+#include "plant.h"
+
+Zombies::Zombies(QString name, bool isStand, QGraphicsScene *scene, QObject *parent)
+    : m_name(name), isStand(isStand)
 {
-    MainWindow::ZombiesNum ++;
-    this->isDead = false;
-    this->isDelete = false;
-    this->setParent(parent);
-    this->m_name = name;
-    this->isStand = isStand;
-    this->isEating = false;
-    this->isLeft = false;
-
-    //设置碰撞tip标签
-    this->setToolTip("zombie");
-
-    this->setCartoonPixmap();
-    this->setZValue(93);
+    MainWindow::ZombiesNum++;
+    setParent(parent);
+    setToolTip("zombie");
+    setZValue(93);
     scene->addItem(this);
 
-    //强制刷新
-    QTimer * update = new QTimer;
-    connect(update,&QTimer::timeout,this,[=](){
-        this->update();
+    initZombieStats();
 
-    });
-    update->start(60);
-    if(!this->isStand)
-    {
-        this->goWalk();
-        QMap<QString,int> speed = {{"normalZombie",79}};
-        this->cartoonSpeed = speed.value(this->m_name);
-    }
-    else
-    {
-        this->cartoonSpeed = 86;
-    }
-    QMap<QString,int> eat = {{"normalZombie",12}};
-    QMap<QString,int> healthMap = {{"normalZombie",10}};
-    this->allEatcartoonNum = eat.value(this->m_name);
-    this->health = healthMap.value(this->m_name);
-
-    this->loadZombieCartoon();
+    // 默认行走动画
+    if (m_name == "bucketZombie")
+        setMovie(":/image/zombie/BucketZombieWalk.gif");
+    if (m_name == "normalZombie")
+        setMovie(":/image/zombie/ZombieWalk2.gif");
+    if (m_name == "ConeZombie")
+        setMovie(":/image/zombie/ConeZombieWalk.gif");
+    if (m_name == "footballZombie")
+        setMovie(":/image/zombie/FootballZombieWalk.gif");
+    currentState = "walk";
 }
 Zombies::~Zombies()
 {
-    if(this->isStand)
-    {
-        MainWindow::ZombiesNum --;
-    }
+    if (isStand)
+        MainWindow::ZombiesNum--;
+    if (movie)
+        delete movie;
 }
-void Zombies::paint(QPainter *painter, const QStyleOptionGraphicsItem *option, QWidget *widget)
+
+void Zombies::initZombieStats()
 {
-    painter->drawPixmap(-23,0,this->zombiePixmap);
+    static const QMap<QString, int> health = {
+        {"normalZombie", 10},
+        {"bucketZombie", 15},// 铁桶僵尸血量15
+        {"ConeZombie", 12},
+        {"footballZombie", 12}
+    };
+    healthPoints = health.value(m_name, 10); // 默认10
+}
 
-
-    //绘制碰撞
-    //        painter->setPen(Qt::red); // 设置边框颜色
-    //        painter->setBrush(Qt::NoBrush); // 不填充颜色
-    //        painter->drawRect(boundingRect()); // 使用boundingRect()获取图形项的边界矩形并绘制
+void Zombies::paint(QPainter *painter, const QStyleOptionGraphicsItem *, QWidget *)
+{
+    if (movie)
+        painter->drawPixmap(-23, 0, movie->currentPixmap());
 }
 
 QRectF Zombies::boundingRect() const
 {
-    return QRectF(0,20,130,130);
-}
-void Zombies::setCartoonPixmap()
-{
-    QMap<QString,int> zombiePix;
-    if(this->isStand)
-    {
-        zombiePix = {{"normalZombie",20}};
-    }
-    else
-    {
-        zombiePix = {{"normalZombie",20}};
-    }
-    this->allcantoonNum = zombiePix.value(this->m_name);
-}
-void Zombies::loadZombieCartoon()
-{
-    this->cartoonTimer = new QTimer(this);
-    this->cartoonNum = 1;
-    this->deadCartoonNum = 0;
+    if (m_name == "coneZombie") {
 
-    connect(cartoonTimer, &QTimer::timeout, this, [=]() {
+        return QRectF(90, 60, 40, 80);
+    }
+    else if (m_name == "footballZombie"){
+        return QRectF(15, 60, 40, 80);
+    }
+    else {
+        // 普通僵尸、铁桶僵尸等
+        return QRectF(55, 70, 40, 80);
+    }
+}
 
-        // 僵尸死亡动画逻辑
-        if (this->isDead)
+void Zombies::setMovie(const QString &path)
+{
+    if (movie) {
+        movie->stop();
+        movie->deleteLater();
+    }
+
+    movie = new QMovie(path);
+    connect(movie, &QMovie::frameChanged, this, [=]() { update(); });
+    movie->start();
+}
+
+/* ===========================
+   行为逻辑
+   =========================== */
+void Zombies::doEating()
+{
+    if (!isEating && !isDead)
+        isEating = true;
+}
+
+void Zombies::eatOver()
+{
+    if (isEating)
+        isEating = false;
+
+}
+
+void Zombies::lessHealth()
+{
+    if (--healthPoints <= 0)
+        dead();
+}
+
+void Zombies::dead()
+{
+    if (!isDead) {
+        isDead = true;
+        isEating = false;
+        QTimer::singleShot(5000, this, [=]() { delete this; }); // 延迟删除
+    }
+}
+
+/* ===========================
+   游戏刷新逻辑
+   =========================== */
+void Zombies::advance(int phase)
+{
+    if (!phase)
+        return;
+
+    updateAnimation();
+
+    if (isDead)
+        return;
+
+    QList<QGraphicsItem *> items = collidingItems();
+    bool touchingPlant = false;
+
+    for (QGraphicsItem *item : items)
+    {
+        if (item->type() == Plant::Type)
         {
-            // 当死亡帧播放完（假设共 9 帧：0~8）
-            if (this->deadCartoonNum > 8)
+            Plant *plant = dynamic_cast<Plant *>(item);
+
+            if (plant && plant->hp > 0)
             {
-                // MainWindow::ZombiesNum--;
-                this->setPos(3000, 1); // 移出屏幕
+                touchingPlant = true;
+                doEating();
 
-                QTimer::singleShot(10000, [=]() {
-                    if (this->isDelete)
-                        return;
-                    delete this;
-                });
-                return;
+                // 攻击力：足球僵尸更强
+                double damage = (m_name == "footballZombie") ? 1.5 : 1;
+                plant->hp -= damage;
+                break;
             }
+        }
+    }
 
-            // 拼出资源路径，比如 ":/image/zombie/zombie_die/ZombieDie_3.png"
-            this->zombiePixmap = QString(":/image/zombie/zombie_die/ZombieDie_%0.png")
-                                     .arg(this->deadCartoonNum);
+    // 没有植物可吃就继续往前走
+    if (!touchingPlant && !isStand)
+    {
+        eatOver();
+        updateAnimation();
 
-            this->deadCartoonNum++;
-            this->update(); // 触发重绘
-            return;
-        }
-        if (this->isEating)
-        {
-            QString path = QString(":/image/zombie_eat/ZombieAttack_%0.png")
-            .arg(this->cartoonNum);
-            this->zombiePixmap = path;
-            this->cartoonNum++;
-
-            if (this->cartoonNum > this->allcantoonNum)
-                this->cartoonNum = 0;
-            return;
-        }
-        if(this->isStand)
-        {
-            this->zombiePixmap = this->zombiePixmap + "stand/";
-        }
-        else
-        {
-            this->zombiePixmap = this->zombiePixmap + "walk/";
-        }
-        // 3️⃣ 僵尸行走动画
-        QString path = QString(":/image/zombie/Zombie_%0.png")
-                           .arg(this->cartoonNum);
-        this->zombiePixmap = path;
-        this->cartoonNum++;
-        if(this->cartoonNum == this->allcantoonNum + 1)
-        {
-            this->cartoonNum = 1;
-        }
-    });
-    cartoonTimer->start(this->cartoonSpeed);
-}
-void Zombies::goWalk()
-{
-    this->walkTimer = new QTimer(this);
-    connect(walkTimer,&QTimer::timeout,this,[=](){
-        this->setPos(this->x()-1, this->y());
-    });
-    walkTimer->start(126);
-
-}
-void Zombies::doEating(){
-    //停止走路
-    this->walkTimer->stop();
-    this->isEating = true;
-}
-void Zombies::eatOver(){
-    this->walkTimer->start(126);
-    this->isEating = false;
-}
-void Zombies::lessHealth(){
-    this->health --;
-    if(this->health==0){
-        this->dead();
+        // 移动速度：足球僵尸更快
+        double speed = (m_name == "footballZombie") ? 1.0 : 0.5;
+        setPos(x() - speed, y());
     }
 }
-void Zombies::dead(){
-    this->isEating = false;
-    this->walkTimer->stop();
-    this->isDead=true;
+
+void Zombies::updateAnimation()
+{
+    QString newState;
+    if (isDead)
+        newState = "die";
+    else if (isEating)
+        newState = "eat";
+    else
+        newState = "walk";
+
+    if (newState != currentState)
+    {
+        currentState = newState;
+        if (m_name == "normalZombie"){
+        if (newState == "die")
+            setMovie(":/image/zombie/zombie_die/ZombieDie.gif");
+        else if (newState == "eat")
+            setMovie(":/image/zombie_eat/ZombieAttack.gif");
+        else
+            setMovie(":/image/zombie/ZombieWalk2.gif");}
+
+        if (m_name == "bucketZombie"){
+
+            if (newState == "die")
+                setMovie(":/image/zombie/zombie_die/ZombieDie.gif");
+            else if (newState == "eat")
+                setMovie(":/image/zombie_eat/BucketZombieAttack.gif");
+            else
+                setMovie(":/image/zombie/BucketZombieWalk.gif");}
+    if (m_name == "ConeZombie"){
+
+        if (newState == "die")
+            setMovie(":/image/zombie/zombie_die/ZombieDie.gif");
+        else if (newState == "eat")
+            setMovie(":/image/zombie_eat/ConeZombieAttack.gif");
+        else
+            setMovie(":/image/zombie/ConeZombieWalk.gif");}
+    if (m_name == "footballZombie"){
+
+        if (newState == "die")
+            setMovie(":/image/zombie/zombie_die/FootballZombieDie.gif");
+        else if (newState == "eat")
+            setMovie(":/image/zombie_eat/FootballZombieAttack.gif");
+        else
+            setMovie(":/image/zombie/FootballZombieWalk.gif");}
 }
-
-
-
+}
